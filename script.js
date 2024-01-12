c71ff5b438ff7089dc6eed1e2fb9d3e0918b31ba
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-01-12 16:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+
+function uppercase() {
+	let field = document.getElementById("fname");
+	field.value = field.value.toUpperCase();
+}
+let field = document.getElementById("fname");
+field.addEventListener("blur", uppercase)
+
+
+
+
